55447ebce0a10cb5d768e7d0a9b2f45f6c64d9e2
Julio <juliow@live.com>
2025-08-25 22:16:35 -0300

    Correções

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,6 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
-let audio = new Audio("./src/tunes.css")
+let audio = new Audio("./src/tunes/;.wav")
 
 const playTune = (key) => {
     audio.src = `src/tunes/${key}.wav`
